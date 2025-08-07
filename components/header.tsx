42e3cf82741054ecd5d308c97edd943e4ba99124
unknown <lee.eunseong7856@gmail.com>
2025-08-07 08:15:15 -0500

    make shadow more prominent

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
   }
 
   return (
-    <header className="bg-white shadow-sm sticky top-0 z-50">
+    <header className="bg-white shadow-md sticky top-0 z-50">
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           {/* Logo */}
